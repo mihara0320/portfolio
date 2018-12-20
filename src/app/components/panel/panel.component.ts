6b8e83ff18efe8d080105b60324bebc49ae41450
Masaki Ihara <m.ihara@lexit.com>
2018-12-20 14:30:18 +0200

    wip

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -10,9 +10,19 @@ export class PanelComponent implements OnInit {
   @HostBinding('style.background-image')
   backgroundImage = null;
 
+  @Input() isScreen = false;
+  @HostBinding('class.screen') screen = false;
   constructor() { }
 
   ngOnInit() {
     this.backgroundImage = 'url(' + this.image + ')';
+    this.screen = this.isScreen;
+  }
+
+  paddingClass() {
+    return {
+      'padding_35': this.isScreen,
+      'padding_20': !this.isScreen
+    };
   }
 }
