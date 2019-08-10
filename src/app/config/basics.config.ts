8c669491c1045f4825687eb6ff9ceeaf9f233e91
Masaki <masaki0320@icloud.com>
2019-08-10 16:28:02 +0300

    new projects add

--- src/app/config/basics.config.ts
+++ src/app/config/basics.config.ts
@@ -2,17 +2,17 @@ import { IBasics } from '../models';
 
 export const BASICS: IBasics = {
     name: 'Masaki Ihara',
-    title1: 'Blockchain specialist',
-    title2: 'Full-stack developer / Blockchain specialist',
+    title1: 'Software Engineer',
+    title2: 'Software Engineer / Full-stack developer',
     country: 'Estonia',
     city: 'Tallinn',
     bio:
-    `I am a professional software developer with passion for blockchain.
-    I have worked for several startups in the past, and currently I have been working on M&A blockchain project.
-    The amazing complexities of developing highly sofisticated solutions have been amusing.
-    Also, I am a freelance full-stack developer and enjoy diversity of projects.`,
+    `I am a professional software developer with passion for full-stack development.
+     I have worked for multiple startups in the past.
+     The amazing complexities of developing highly sofisticated solutions have been amusing.
+     Also, I am a freelance developer and enjoy diversity of projects.`,
     email: 'mxxxxi.ixxxxa@gmail.com',
     subject: '<<Message from Portfolio>>',
-    motd1: 'Need a blockchain specialist / full-stack developer?',
+    motd1: 'Need help for full-stack development?',
     motd2: 'I am actively looking for new challenges'
 };
